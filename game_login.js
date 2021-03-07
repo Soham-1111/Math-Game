964a26c19e8903835613dfb7a3cc0eafb4caee46
Soham-1111 <73647733+Soham-1111@users.noreply.github.com>
2021-03-07 13:15:43 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -3,7 +3,9 @@ function addUser(){
  player2_name= document.getElementById("player2_input").value; 
  
  localStorage.setItem("player1",player1_name );
- localStorage.setItem("playerw",player1_name );
+ localStorage.setItem("player2",player2_name );
 
  window.location="game_page.html";
+
+
 }
