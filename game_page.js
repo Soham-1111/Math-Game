2dee9e4c4e3e521525a773f61c988a1e23c4f11c
Soham-1111 <73647733+Soham-1111@users.noreply.github.com>
2021-03-10 12:57:08 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -16,3 +16,20 @@ document.getElementById("score2").innerHTML= player2_score;
 
 document.getElementById("questionturn").innerHTML= "Question Turn :"+player1_name;
 document.getElementById("answerturn").innerHTML= "Answer Turn :"+player2_name;
+
+function send(){
+    number1= document.getElementById("number1").value;
+    number2= document.getElementById("number2").value;
+    actual_answer= parseInt(number1) * parseInt(number2);
+    console.log(actual_answer);
+
+    question_number="<h4>" + number1 + "X" + number2 + "</h4>";
+    input_box= "<input id='answer' type='text' class='form-control'>";
+    send_button= "<button id='check' onclick='check()' class='btn btn-info'>Check</button>" ;
+    row= question_number + input_box + send_button;
+    document.getElementById("output").innerHTML= row;
+
+    document.getElementById("number1").value= "";
+    document.getElementById("number2").value= "";
+
+}
